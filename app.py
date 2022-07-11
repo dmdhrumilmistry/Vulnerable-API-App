5f2554251b7b2a201ca12591c793dccc81e37aa6
Dhrumil Mistry <56185972+dmdhrumilmistry@users.noreply.github.com>
2022-07-11 11:44:38 +0530

    create UserView
    add fake data to database if empty

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+from faker import Faker
 from flask import Flask, jsonify
-from flask_jwt_extended import JWTManager, jwt_required
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token
 from flask_restful import Api
-from models import db
-from views import UsersView
+from models import UserModel, db
+from views import UsersView, UserView
+from random import randint
 
 import json
 
@@ -26,13 +28,35 @@ api = Api(app)
 def configure_app():
     # init db
     db.create_all()
+    total_users = len(UserModel.query.all())
 
-    # TODO: dump fake data into database
+    # dump fake data into database if database is empty
+    if total_users == 0:
+        fake = Faker()
+        for _ in range(500):
+            first_name: str = fake.first_name()
+            last_name: str = fake.last_name()
+
+            # create unique email and token
+            email = f'{first_name.lower()}.{last_name.lower()}.{randint(0,99999)}@vuln-api-app.com'
+            token = create_access_token(identity=email, expires_delta=False)
+
+            # create user and update database
+            new_user = UserModel(
+                first_name=first_name,
+                last_name=last_name,
+                email=email,
+                jwt_token=token
+            )
+            db.session.add(new_user)
+            db.session.commit()
+
 
 @app.route('/home')
 @jwt_required()
 def home():
-    return jsonify({'message':'success'})
+    return jsonify({'message': 'success'})
+
 
 @app.route("/api")
 def flask_main():
@@ -45,6 +69,7 @@ def flask_main():
 
 # add resources to the flask application using api
 api.add_resource(UsersView, '/api/users')
+api.add_resource(UserView, '/api/user/<int:id>')
 
 if __name__ == '__main__':
     app.run('localhost', 3000)
--- views.py
+++ views.py
@@ -1,9 +1,12 @@
+from sqlalchemy import delete
 from models import db
 from flask import request, jsonify, make_response
 from flask_restful import Resource
 from flask_jwt_extended import create_access_token
 from models import UserModel
 
+
+# for all users
 class UsersView(Resource):
     def get(self):
         users = UserModel.query.all()
@@ -34,19 +37,52 @@ class UsersView(Resource):
             user_present = False
         else:
             message = {'message': 'email already used'}
-            
+
         # validate data and if user is not present then
-        # create and store new user 
+        # create and store new user
         if check_data(first_name) and check_data(last_name) and check_data(email) and not user_present:
             new_user = UserModel(
                 first_name=first_name,
                 last_name=last_name,
                 email=email,
-                jwt_token=create_access_token(identity=email)
+                jwt_token=create_access_token(
+                    identity=email, expires_delta=False)
             )
             db.session.add(new_user)
             db.session.commit()
             message = new_user.json(hide_sensitive_info=False)
             status_code = 201
+
+        return make_response(jsonify(message), status_code)
+
+# for specific user
+class UserView(Resource):
+    def get(self, id: int):
+        # if request header has Hide-Info header as 1 disclose sensitive information for that user
+        hide_sensitive_info = request.headers.get('Hide-Info', 1, type=int)
+        hide_sensitive_info = bool(hide_sensitive_info)
+
+        user: UserModel = UserModel.query.filter_by(id=id).first()
+
+        message = {'message': 'user not found'}
+        status_code = 404
+
+        if user:
+            message = user.json(hide_sensitive_info=hide_sensitive_info)
+            status_code = 200
+
+        return make_response(jsonify(message), status_code)
+
+    def delete(self, id: int):
+        user: UserModel = UserModel.query.filter_by(id=id).first()
+
+        message = {'message': 'user not found'}
+        status_code = 404
+
+        if user:
+            db.session.delete(user)
+            db.session.commit()
+            message = {'message': 'user deleted'}
+            status_code = 200
 
         return make_response(jsonify(message), status_code)
